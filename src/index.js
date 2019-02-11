581092f662778daad015d36f7fd874c2ab85a897
Rodrigo Nappi <rodrigo_nappi@hotmail.com>
2019-02-11 11:17:19 -0200

    Persisting state to localStorage

--- src/index.js
+++ src/index.js
@@ -2,17 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, compose } from 'redux';
+import throttle from 'lodash/throttle';
 
 import App from './components/App';
 import reducers from './reducers';
+import { loadState, saveState } from './localStorage';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const persistedState = {/* ... */};
+const persistedState = loadState();
 const store = createStore(
     reducers,
     persistedState,
     composeEnhancers()
 );
+
+// saves only the todos prop from state
+store.subscribe(throttle(() => {
+    saveState({
+        todos: store.getState().todos
+    });
+}, 1000)); // saveState will not be call before the time defined on parameter
 
 ReactDOM.render(
     <Provider store={store}>
